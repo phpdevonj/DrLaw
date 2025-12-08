857af329fa5053b2f5a6257ebb7234a4921445bb
lalit <lalit.sain@ongraph.com>
2025-12-08 15:51:02 +0530

    fixed issue for profile update api error

--- app/Http/Requests/UserRequest.php
+++ app/Http/Requests/UserRequest.php
@@ -39,8 +39,8 @@ class UserRequest extends FormRequest
         $rules = [
             'username'  => 'required|unique:users,username,'.$user_id,
             'email'     => 'required|email|unique:users,email,'.$user_id,
-            'contact_number' => 'required|max:20',
-            'full_contact_number' => 'required|unique:users,contact_number,' . $user_id,
+            'contact_number' => 'nullable|max:20',
+            'full_contact_number' => 'nullable|unique:users,contact_number,' . $user_id,
             'profile_image' => 'nullable|image|mimes:jpeg,png,jpg,gif,webp|max:1024',
         ];
 
@@ -54,7 +54,7 @@ class UserRequest extends FormRequest
             'profile_image.image' => 'The profile image must be a valid image file.',
             'profile_image.mimes' => 'Allowed image types: jpeg, png, jpg, gif, webp.',
             'profile_image.max' => 'The profile image must not be larger than 1MB.',
-            'contact_number.required' => 'Mobile number is required.',
+            //'contact_number.required' => 'Mobile number is required.',
             'full_contact_number.unique' => 'This mobile number has already been taken.',
         ];
     }
